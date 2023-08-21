faeee5035b631d11b439ecfc7b1225588d6933a5
Rustam Dzhaksimov <dzhaksimov.rustam@gmail.com>
2023-08-21 19:26:48 +0300

    feat: increase ping interval

--- lib/src/transports/NativeTransport.dart
+++ lib/src/transports/NativeTransport.dart
@@ -65,7 +65,7 @@ class Transport extends TransportInterface {
     _ws = IOWebSocketChannel.connect(
       Uri.parse(_url),
       protocols: ['protoo'],
-      pingInterval: const Duration(milliseconds: 500),
+      pingInterval: const Duration(seconds: 1),
     );
 
     await _ws?.ready.then((value) {
